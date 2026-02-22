bf4a6efa7efd6e44f89a8089f52bbaa695e2c037
JEIDY MARIBEL FAVICELA USHCO <jm.favicelau@uea.edu.ec>
2026-02-22 15:26:31 -0500

    Actualización del proyecto con plantillas dinámicas.

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "Bienvenido al Sistema de Inventario - Cyber Fenix Tecnología"
+    return render_template('index.html')
 
 @app.route('/item/<codigo>')
 def item(codigo):
-    return f"Producto con código {codigo} registrado en el inventario de Cyber Fenix"
+    return render_template('item.html', codigo=codigo)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/productos')
+def productos():
+    lista_productos = [
+        {"codigo": "A1", "nombre": "Laptop"},
+        {"codigo": "B2", "nombre": "Mouse"},
+        {"codigo": "C3", "nombre": "Teclado"}
+    ]
+    return render_template('productos.html', productos=lista_productos)
 
 if __name__ == '__main__':
     app.run(debug=True)
